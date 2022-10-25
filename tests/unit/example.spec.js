ac512163186af22882c00a2ae1a71048ae3f4116
AirMaster45 <116429354+AirMaster45@users.noreply.github.com>
2022-10-25 14:11:28 +0530

    Test Extended

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,6 +1,8 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import BugResolver from "V3.0"
+
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -10,4 +12,12 @@ describe("HelloWorld.vue", () => {
     });
     expect(wrapper.text()).to.include(msg);
   });
+  
+  if(bug==true){
+    console.log("Bug found");
+    bug.resolve();     
+  }else{
+    const msg = "Verified";
+    pass;
+  }
 });
